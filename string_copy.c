f06c844660926736ed31ff44cb6a1586b1e2c461
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2026-04-28 18:03:37 +0600

    String copy using strcpy

--- string_copy.c
+++ string_copy.c
@@ -1,14 +1,25 @@
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+//     char a[101], b[101];
+//     scanf("%s %s,", &a, &b);
+//     int length = strlen(b);
+//     for (int i = 0; i <= length; i++)
+//     {
+//         a[i] = b[i];
+//     }
+//     printf("%s %s", a, b);
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 int main()
 {
-    char a[10], b[10];
+    char a[101], b[101];
     scanf("%s %s,", &a, &b);
-    int length = strlen(b);
-    for (int i = 0; i <= length; i++)
-    {
-        a[i] = b[i];
-    }
+    strcpy(a, b);
     printf("%s %s", a, b);
     return 0;
 }
